b8fcf3de4b65ac447c0f912bd498a23c701af29d
David O'Neill <davidoneill@b-10-27-67-55.dynapool.vpn.nyu.edu>
2024-05-09 16:38:15 -0400

    Fix plotting? Let's try again

--- Angular_Momentum_Plot.py
+++ Angular_Momentum_Plot.py
@@ -2,13 +2,13 @@ import sys
 import numpy as np 
 import pickle as pk
 import matplotlib.pyplot as plt 
-
-sys.path.insert(1,"/groups/astro/davidon/sailfish/")
 import sailfish
+import os
 
 Momentum_Timeseries  = True
 Accretion_Timeseries = True
 Steady_State         = True
+SemiMajorAxis        = True
 
 def load_checkpoint(filename, require_solver=None):
     with open(filename, "rb") as file:
@@ -23,7 +23,6 @@ def E_from_M(M, e=1.0):
 class DavidTimeseries:
 
     def __init__(self, chkpt):
-        #print(load_checkpoint(chkpt).keys())
         ts = load_checkpoint(chkpt)['timeseries']
         self.time           = np.array([s[ 0] for s in ts])
         self.semimajor_axis = np.array([s[ 1] for s in ts])
@@ -63,77 +62,105 @@ class DavidTimeseries:
       
 
 if __name__ == '__main__':
-    filepath = '/lustre/astro/davidon/Storage/sfish-test/Retrograde_Circular_Iso_Test/e0/'
-    filename = filepath + 'chkpt.%04d.pk'%(int(sys.argv[1]))
-    
-    Savepath = filepath.replace("/lustre/astro/davidon/Storage/sfish-test/","/groups/astro/davidon/sailfish/Outputs/") 
+    filename         = sys.argv[1]
+    CurrentTime      = load_checkpoint(filename)["time"]/ 2 / np.pi
+    #print(CurrentTime)
+    ts               = DavidTimeseries(filename)
+    Model_Parameters = load_checkpoint(filename)['model_parameters']
 
     if Momentum_Timeseries:
-        ts   = DavidTimeseries(filename)
         j0   = ts.jdisk
         plt.plot(ts.time, 1-ts.jdisk / j0, label=r'$\Delta j_\mathrm{disk}/j_0 $',c = 'blue')
         plt.plot(ts.time, ts.binary_delta_j / j0, label='binary torque',c = 'black')
         plt.plot(ts.time, ts.buffer_delta_j / j0, label='buffer torque',c = 'green')
         plt.plot(ts.time, 1-ts.total_angular_momentum / j0, label='1-total/j0',linestyle = 'dashed')
         plt.xlabel('time')
-        #plt.ylabel()
         YBounds = max(np.max(np.abs(1-ts.jdisk / j0)), np.max(np.abs(ts.binary_delta_j / j0)), np.max(np.abs(ts.buffer_delta_j / j0)))
-        plt.ylim([-YBounds/10,YBounds/10])
+        plt.ylim([-YBounds/3,YBounds/3])
         plt.legend()
-        savename = "AngularMomentum.%04d.png"%(int(sys.argv[1]))
-        plt.savefig(Savepath + savename, dpi=400)
+        savename = os.getcwd() + "/Outputs/AngularMomentum.%04d.png"%(CurrentTime)
+        plt.savefig(savename, dpi=400)
 
     if Accretion_Timeseries:
-        Model_Parameters       = load_checkpoint(filename)['model_parameters']
         viscosity              = Model_Parameters['nu']
         Initial_Density        = Model_Parameters['initial_sigma']
         Steady_State_Accretion = 3 * np.pi * viscosity * Initial_Density
-
-        ts   = DavidTimeseries(filename)
-        mdot = ts.mdot1+ts.mdot2
-
+        mdot                   = ts.mdot1+ts.mdot2
         plt.figure()
         plt.plot(ts.time,mdot,label='mdot',linewidth = 1)
         YBounds = np.max(np.abs(mdot))
-        plt.ylim([-YBounds/5,0])
+        plt.ylim([-YBounds/3,YBounds/10])
         plt.xlabel('time')
         plt.legend()
-        savename = "AccretionRate.%04d.png"%(int(sys.argv[1]))
-        plt.savefig(Savepath + savename, dpi=400)
+        savename = os.getcwd() +  "/Outputs/AccretionRate.%04d.png"%(CurrentTime)
+        plt.savefig(savename, dpi=400)
 
     if Steady_State:
-        Model_Parameters  = load_checkpoint(filename)['model_parameters']
-        viscosity         = Model_Parameters['nu']
-        Initial_Density   = Model_Parameters['initial_sigma']
-        CheckpointCadence = load_checkpoint(filename)['driver'].events['checkpoint'].interval
-
-        ts                     = DavidTimeseries(filename)
+        viscosity              = Model_Parameters['nu']
+        Initial_Density        = Model_Parameters['initial_sigma']
+        CheckpointCadence      = load_checkpoint(filename)['driver'].events['checkpoint'].interval
         Steady_State_Accretion = 3 * np.pi * viscosity * Initial_Density
         mdot                   = ts.mdot1+ts.mdot2
-        Number_of_Orbits       = 100
+        Number_of_Orbits       = 10
 
-        
         plt.figure()
         Later_Times   = ts.time[len(ts.time)-Number_of_Orbits*10:len(ts.time)-1]
         Later_Torques = ts.torque_g[len(ts.time)-Number_of_Orbits*10:len(ts.time)-1]
         Later_mdot    = mdot[len(ts.time)-Number_of_Orbits*10:len(ts.time)-1]
         plt.plot(Later_Times,Later_Torques/Later_mdot, c= 'black', label = 'Unitless Torque')
         plt.plot(Later_Times,Later_mdot/Steady_State_Accretion,c = 'red',label = r'$\dot{M}/M_0$ ')
-        #plt.plot(ts.time,ts.torque_g/mdot,label='Unitless gravitational torque')
         plt.xlabel('time')
         plt.legend()
-        savename = "SteadyState_TorqueAccretion.%04d.png"%(int(sys.argv[1]))
-        plt.savefig(Savepath + savename, dpi=400)
+        savename = os.getcwd() +  "/Outputs/SteadyState_TorqueAccretion.%04d.png"%(CurrentTime)
+        plt.savefig(savename, dpi=400)
 
-    
         plt.figure()
         Split_Array  = np.array_split([Later_Torques[i]/Later_mdot[i] for i in range(0,Number_of_Orbits*10 -1)],Number_of_Orbits)
         Mean_torques = [np.mean(Split_Array[i]) for i in range(0,Number_of_Orbits)]
-        #print(Mean_torques)
-        Final_Orbits = np.arange(int(sys.argv[1]) * CheckpointCadence - Number_of_Orbits,int(sys.argv[1])* CheckpointCadence,1)
+        Final_Orbits = np.arange(np.round(CurrentTime) - Number_of_Orbits,np.round(CurrentTime),1)
         plt.plot(Final_Orbits,Mean_torques,c = 'black',label = 'Orbit Averaged Torque')
         plt.axhline(y = np.mean(Mean_torques), c = 'red',label = 'Global Mean Torque')
         plt.xlabel('Time [t/P]')
         plt.legend()
-        savename = "MeanTorque_FinalOrbits.%04d.png"%(int(sys.argv[1]))
-        plt.savefig(Savepath + savename, dpi=400)
+        savename = os.getcwd() +  "/Outputs/MeanTorque_FinalOrbits.%04d.png"%(CurrentTime)
+        plt.savefig(savename, dpi=400)
+
+    if SemiMajorAxis:
+        plt.figure()
+        plt.plot(ts.time,ts.semimajor_axis, label = 'SemiMajor Axis')
+        plt.plot(ts.time,ts.eccentricity, label = 'Eccentricity')
+        plt.xlabel('Time')
+        plt.ylabel('Orbital Elements')
+        plt.legend()
+        savename = os.getcwd() +  "/Outputs/OrbitalElements.%04d.png"%(CurrentTime)
+        plt.savefig(savename, dpi=400)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
