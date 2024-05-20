88c61685307bfd6ff3b64401988d7c1c6aa89951
David O'Neill <davidoneill@a-10-27-8-190.dynapool.vpn.nyu.edu>
2024-05-20 15:48:11 -0400

    Add cavity properties and output file

--- CavityCapture.py
+++ CavityCapture.py
@@ -98,7 +98,6 @@ def main_cbdiso_2d(chkpt,points):
 	apses_y  = CavityProperties["Apsidal_Y_Positions"]
 	arg_aps  = CavityProperties["Cavity_Slope_Radians"]
 
-
 	def Parameterisation(a,e,f,w):
 		r = a * (1-e**2) / (1+e*np.cos(f-w))
 		return apses_x[0] - a*(1-e)*np.cos(w) + r * (np.cos(f)), apses_y[0] -a*(1-e)*np.sin(w) + r * (np.sin(f))
@@ -106,7 +105,7 @@ def main_cbdiso_2d(chkpt,points):
 	Position_List = np.array([Parameterisation(a_cavity,e_cavity,i,arg_aps) for i in np.linspace(0,2*np.pi,100)]).reshape(100,2)
 	plt.plot(Position_List[:,0],Position_List[:,1],c = 'black', linestyle = 'dashed', linewidth = 2)
 	
-	plt.title('Orbit',chkpt["time"] / 2 / np.pi)
+	fig.suptitle('Orbit %g'%(chkpt["time"] / 2 / np.pi))
 	plt.show()
 
 
@@ -115,17 +114,68 @@ def main_cbdiso_2d(chkpt,points):
 
 if __name__ == "__main__":
 	from pathlib import Path
-	directory = Path(sys.argv[1])
+	import os
+	import fnmatch
+
+	directory       = Path(sys.argv[1])
+	Time_Snapshots  = []
+	Semi_Major_Axis = []
+	Eccentricity    = []
+	Argument_Apses  = []
 	for arg in directory.iterdir():
-		if arg.suffix == '.pk':
+		if fnmatch.fnmatch(arg, '*chkpt*.pk'):
+		#if arg.suffix == '.pk':
+			print('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~')
+			print('Reading in',arg)
 			chkpt         = load_checkpoint(arg)
 			contour_lines = CavityContour(chkpt)
-			print('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~')
-			print(arg,'has cavity parameters',MaxDist(contour_lines))
+			cavity_properties = MaxDist(contour_lines)
+			print('Which has cavity parameters',cavity_properties)
+			Semi_Major_Axis.append(cavity_properties["SemiMajorAxis"])
+			Eccentricity.append(cavity_properties["Eccentricity"])
+			Argument_Apses.append(cavity_properties["Cavity_Slope_Radians"])
+			Time_Snapshots.append(chkpt["time"] / 2 / np.pi)
 			if Plot:
 				main_cbdiso_2d(chkpt,contour_lines)
 			else:
 				pass
+
+	
+	
+
+	lists = list(zip(Time_Snapshots, Semi_Major_Axis, Eccentricity, Argument_Apses))
+	sorted_lists = sorted(lists, key=lambda x: x[0])
+	sorted_times, sorted_SMA, sorted_ecc, sorted_Apses = zip(*sorted_lists)
+
+	Cavity_Properties = {
+	"time": list(sorted_times),
+	"SemiMajorAxis": list(sorted_SMA),
+	"Eccentricity": list(sorted_ecc),
+	"ApsidalInclination": list(sorted_Apses)
+	}
+
+	outdir = sys.argv[2]
+	FileName = f"CavityProperties.{int(np.round(10*Time_Snapshots[-1],2)):04d}.pk"
+	Path(outdir).mkdir(parents=True, exist_ok=True)
+	FileName = os.path.join(outdir, FileName)
+
+	with open(FileName, "wb") as cvt:
+		pk.dump(Cavity_Properties, cvt)
+
+	fig, ax = plt.subplots(figsize=[12, 9])
+	plt.title('Cavity Properties')
+	plt.plot(sorted_times,sorted_SMA, c = 'brown', linewidth = 2, label = r'Semi Major Axis $[a_0]$')
+	plt.plot(sorted_times,sorted_ecc, c = 'blue', linestyle = 'dashed',linewidth = 2, label = 'Eccentricity')
+	plt.plot(sorted_times,sorted_Apses, c = 'silver', linestyle = 'dotted',linewidth = 2, label = 'Apsidal Inclination (Radians)')
+	plt.scatter(sorted_times,sorted_SMA, c = 'brown', s = 50)
+	plt.scatter(sorted_times,sorted_ecc, c = 'blue', s = 50)
+	plt.scatter(sorted_times,sorted_Apses, c = 'silver', s = 50)
+	plt.xlabel(r'Time $2\pi\Omega_0^{-1}$')
+	plt.legend()
+	FigDirectory =  sys.argv[2]
+	pngname = FigDirectory + f"{'/CavityProperties'}.{int(np.round(10*Time_Snapshots[-1],2)):04d}.png"
+	print(pngname)
+	fig.savefig(pngname, dpi=400)
 	print('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~')
 	exit()
 
