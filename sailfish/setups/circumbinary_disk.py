3dc30650cec9c6f0f6d2b129f6353927c9578d72
David O'Neill <davidoneill@a-10-27-13-117.dynapool.vpn.nyu.edu>
2024-05-30 12:30:53 -0400

    Integrate orbital phase

--- sailfish/setups/circumbinary_disk.py
+++ sailfish/setups/circumbinary_disk.py
@@ -835,7 +835,7 @@ class BinaryInspiral(SetupBase):
             Nstep                    = floor(Inspiral_Progress)
 
             from numpy import trapz
-            Integrated_Orbital_Phase = trapz(self.FixedPhases[0,Nstep], self.inspiral_time_list[0,Nstep], axis=0)
+            Integrated_Orbital_Phase = trapz(self.FixedPhases[0:Nstep], self.inspiral_time_list[0:Nstep], axis=0)
 
             return Integrated_Orbital_Phase + self.inspiral_start_time * self.reference_time_scale
 
