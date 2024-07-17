207b39eaf1a80fdbd6ca230fd3433aeb60d1ed92
David O'Neill <davidoneill@b-10-27-84-198.dynapool.vpn.nyu.edu>
2024-07-17 13:42:41 +0200

    Collect Cavity Pickle Files for multiple decoupling plots

--- CavityCapture.py
+++ CavityCapture.py
@@ -225,7 +225,7 @@ def extract_number_from_pattern(string, pattern):
 
 def Load_Cavity_Files(parent_dir):
 	subdirs = [name for name in os.listdir(parent_dir) if os.path.isdir(os.path.join(parent_dir, name))]
-    print(subdirs)
+	print(subdirs)
 	for in_dir in subdirs:
 		
 		CavityFileName = CavityEvolution(in_dir)
